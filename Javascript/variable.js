25f7f18b45c0860da30ecf14457a042fd2c7d386
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-07-09 23:40:44 +0200

    variable declaration

---
+++ Javascript/variable.js
@@ -0,0 +1,26 @@
+let Name = "Mugisha";
+let age = 25;
+let isStudent = true;
+let address = {
+  city: "Kigali",
+  country: "Rwanda"
+};
+let hobbies = ["reading", "gaming", "hiking"];
+let undefinedVariable;
+let nullVariable = null;
+
+console.log("Name:",Name, "| Type:", typeof Name);
+console.log("Age:", age, "| Type:", typeof age);
+console.log("Is Student:", isStudent, "| Type:", typeof isStudent);
+console.log("Address:", address, "| Type:", typeof address);
+console.log("Hobbies:", hobbies, "| Type:", typeof hobbies);
+console.log("Undefined Variable:", undefinedVariable, "| Type:", typeof undefinedVariable);
+console.log("Null Variable:", nullVariable, "| Type:", typeof nullVariable);
+
+console.log("Type of Name:", typeof Name);
+console.log("Type of Age:", typeof age);
+console.log("Type of Is Student:", typeof isStudent);
+console.log("Type of Address:", typeof address);
+console.log("Type of Hobbies:", typeof hobbies);
+console.log("Type of Undefined Variable:", typeof undefinedVariable);
+console.log("Type of Null Variable:", typeof nullVariable);
